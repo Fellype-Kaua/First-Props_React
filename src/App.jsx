07688dde55be19f036cdefadf708a393b3783c99
Fellype Kauã <94308439+Fellype-Kaua@users.noreply.github.com>
2023-09-08 21:32:57 -0300

    fix - new feature
    correção de alguns bugs visuais;
    Adição do formulário para criação das cartas, tornando assim o projeto mais utilizável, removendo assim a dependência de utilização do código para realizar as alterações no conteúdo.
    Alguns recursos ainda precisam ser adicionados, novas melhorias serão adicionadas no próximo commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,146 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./card/Card.jsx";
 import "./App.css";
 import BlueEyesWhiteDragon from "./card/images/Blue-Eyes.White.Dragon.jpg";
 import levelImg from "./card/images/attributes/level.png";
 import AttributeLight from "./card/images/attributes/LIGHT.png";
+import AttributeDark from "./card/images/attributes/DARK.png";
+import AttributeDivine from "./card/images/attributes/DIVINE.png";
+import AttributeEarth from "./card/images/attributes/EARTH.png";
+import AttributeFire from "./card/images/attributes/FIRE.png";
+import AttributeWater from "./card/images/attributes/WATER.png";
+import AttributeWind from "./card/images/attributes/WIND.png";
 import AttributeSpell from "./card/images/attributes/SPELL.png";
-import AttributeTrap from "./card/images/attributes/TRAP.png"
+import AttributeTrap from "./card/images/attributes/TRAP.png";
 import monsterReborn from "./card/images/monsterReborn.jpeg";
 import solemnJudgment from "./card/images/SolemnJudgment.webp";
 
-
 function App() {
-  return (
-    <div className="App">
+  const [cardVariable, setCardVariable] = useState({
+    level: 1,
+    attack: 0,
+    defense: 0,
+    cardImage: null,
+    cardType: "monster",
+    attribute: AttributeLight,
+  }); // Estado local para o nível
 
-      <Card cardType="monster" cardImg={BlueEyesWhiteDragon} levelImg={levelImg} level="8" attribute={AttributeLight} atk="3000" def="2500"/>
-      
-      <Card cardType="spell" cardImg={monsterReborn} level="0" attribute={AttributeSpell}/>
-      <Card cardType="trap" cardImg={solemnJudgment} level="0" attribute={AttributeTrap}/>
+  function showHideMonsterAtkDef() {
+    const attackForm = document.getElementById("attackForm");
+    const defenseForm = document.getElementById("defenseForm");
+    const levelForm = document.getElementById("levelForm");
+
+    if (attackForm && defenseForm && levelForm) {
+      if (cardVariable.cardType === "monster") {
+        attackForm.style.display = "block";
+        defenseForm.style.display = "block";
+        levelForm.style.display = "block";
+      } else {
+        attackForm.style.display = "none";
+        defenseForm.style.display = "none";
+        levelForm.style.display = "none";
+      }
+    }
+  }
+
+  // Chame a função showHideMonsterAtkDef() dentro de useEffect para garantir que seja chamada após a renderização inicial
+  useEffect(() => {
+    showHideMonsterAtkDef();
+  }, []);
+
+  return (
+    <div className="App" onChange={showHideMonsterAtkDef()}>
+      <section id="createCard">
+        <form>
+          <div id="levelForm">
+            <span>Level</span>
+            <input
+              type="number"
+              id="level"
+              value={cardVariable.level}
+              onChange={(e) =>
+                setCardVariable({
+                  ...cardVariable,
+                  level: parseInt(e.target.value),
+                })
+              }
+              min={1}
+              max={12}
+            />
+          </div>
+          <div id="attackForm">
+            <span>Ataque</span>
+            <input
+              type="number"
+              id="atk"
+              value={cardVariable.attack}
+              min={0}
+              onChange={(e) =>
+                setCardVariable({ ...cardVariable, attack: e.target.value })
+              }
+            />
+          </div>
+          <div id="defenseForm">
+            <span>Defesa</span>
+            <input
+              type="number"
+              id="def"
+              value={cardVariable.defense}
+              min={0}
+              onChange={(e) =>
+                setCardVariable({ ...cardVariable, defense: e.target.value })
+              }
+            />
+          </div>
+
+          <div>
+            <span>Tipo da carta</span>
+            <select
+              name="cardType"
+              id="cardType"
+              onChange={(e) =>
+                setCardVariable({ ...cardVariable, cardType: e.target.value })
+              }
+            >
+              <option value="monster">Monstro</option>
+              <option value="spell">Magia</option>
+              <option value="trap">Armadilha</option>
+            </select>
+          </div>
+
+          <div>
+            <span>Atributo</span>
+            <select name="Attribute" id="attributeForm" onChange={(e) =>
+              setCardVariable({...cardVariable, attribute: e.target.value})
+            }>
+              <option value={AttributeLight}>Luz</option>
+              <option value={AttributeDark}>Trevas</option>
+              <option value={AttributeDivine}>Divino</option>
+              <option value={AttributeEarth}>Terra</option>
+              <option value={AttributeFire}>Fogo</option>
+              <option value={AttributeWater}>Agua</option>
+              <option value={AttributeWind}>Vento</option>
+              <option value={AttributeSpell}>Magia</option>
+              <option value={AttributeTrap}>Armadilha</option>
+            </select>
+          </div>
+
+          <div>
+            <span>Imagem</span>
+            <input type="file" id="cardImagem" />
+          </div>
+          <button>Gerar</button>
+        </form>
+      </section>
+      <Card
+        cardType={cardVariable.cardType}
+        cardImg={BlueEyesWhiteDragon}
+        levelImg={levelImg}
+        level={cardVariable.level}
+        attribute={cardVariable.attribute}
+        atk={cardVariable.attack}
+        def={cardVariable.defense}
+      />
     </div>
   );
 }
